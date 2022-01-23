cd4825f7ebfb40754e7f8c05c317eacdba12e0ce
Zihua Li <i@zihua.li>
2022-01-23 16:25:33 +0800

    Add post banner

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,10 @@ const config = {
   lang: "zh-CN",
   site: "https://zihua.li",
   name: "子骅的个人站",
-  description: "子骅的个人站",
+  description: "偶尔在这里写一些互联网技术、管理等相关的文章。",
   email: "blog@zihua.li",
   twitter: "luinlee",
+  github: "luin",
   nav: [
     // { title: "Posts", link: "/posts" },
     // { title: "Projects", link: "/projects" },
